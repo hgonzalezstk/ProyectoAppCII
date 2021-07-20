e62dc76032a312bed0a57d134058a3e283aefd46
DapozU <85184784+DapozU@users.noreply.github.com>
2021-07-20 13:38:55 -0600

    Delete Daniel directory

--- Daniel/js/controlador-general.js
+++
@@ -1,9 +0,0 @@
-const cambiar_estilo_navegacion = () => {
-    let navegacion = document.getElementById("header-principal");
-
-    if (navegacion.className == "header-principal") {
-        navegacion.className = "header-principal responsive";
-    } else {
-        navegacion.className = "header-principal";
-    }
-}
